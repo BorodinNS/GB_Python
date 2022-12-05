d84769398c9b41dfb7ce17f85766fe5a3306a732
Borodin <Fallenlord48@gmail.com>
2022-12-05 12:57:02 +0300

    Редактирование Homework02

--- Homework/Homework02.py
+++ Homework/Homework02.py
@@ -22,16 +22,16 @@ print(fibbo_list) """
 #3. Дан массив размера N. После каждого отрицательного элемента массива вставьте элемент с нулевым значением.
 
 """ import random
-
-n_list = []
-for i in range(0, (int(input('N: ')))):
-    n_list.append(random.randint(-99, 100))
-print(n_list,end=' => ')
-for i in range(0, len(n_list)+1):
-    if(n_list[i]<0):
-        n_list.insert(i+1,0)
+num = int(input('Введите число: '))
+numbers = []
+for i in range(num):
+    numbers.append(random.randint(-99, 100))
+print(numbers, end = ' => ')
+for i in range(0, len(numbers)+1):
+    if(numbers[i]<0):
+        numbers.insert(i+1,0)
         i+=1
-print(n_list) """
+print(numbers) """
 
 #4. Составьте алгоритм нахождения случайного числа без использования библиотеки random.
 
