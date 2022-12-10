82039af2ae6709037d236ac57023e4da3089a5a0
Borodin <Fallenlord48@gmail.com>
2022-12-10 13:42:33 +0300

    Перенос текстового файла в папку с семинарами

--- Семинары/Seminar04.py
+++ Семинары/Seminar04.py
@@ -5,7 +5,7 @@ print(max(numbers), min(numbers)) """
 
 # 2. Задайте два числа. Напишите программу, которая найдет НОК (наименьшее общее кратное) этих двух чисел.
 # НОК - наименьшее общее кратное, которое делится и на первое, и на второе число.
-num1, num2 = map(int, input().split())
+""" num1, num2 = map(int, input().split())
 
 def search_nod(num1, num2):
     while num1 != 0 and num2 != 0:
@@ -19,7 +19,7 @@ def search_nod(num1, num2):
 def search_nok(num1, num2):
     return num1 * num2 / search_nod(num1, num2)
 
-print(search_nok(num1, num2))
+print(search_nok(num1, num2)) """
 
 # 3. Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
 # Число N вводится пользователем. Позиции хранятся в файле ПозицииN.txt в одной строке одно число
@@ -27,10 +27,10 @@ print(search_nok(num1, num2))
 # Позиции в файл вам нужно программно положить в зависимости от выбранного N: если пользователь введет двойку,
 # то в файле вряд ли будут индексы 5 или 16.
 # В решении должны быть и запись в файл, и чтение из файла.
-""" from random import randint
+from random import randint
 def write_to_file(n):
-    with open ('ПозицииN.txt', 'w') as file:
+    with open ('Семинары/ПозицииN.txt', 'w') as file:
         for i in range(n):
             file.write(f'{randint(-n, n)}\n')
 
-write_to_file(5) """
+write_to_file(5)
